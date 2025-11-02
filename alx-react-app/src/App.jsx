24705dedab5c569dd6de7b444b280d3529ee0efd
Jonathan D. Ntim <jonathankweku2@gmail.com>
2025-11-02 10:54:42 +0000

    Modify App.jsx to add UserProfile with new information

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile 
-        name="Jonathan Darko Ntim" 
-        age="30" 
-        bio="Love to travel and being around the Deaf community." 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
       />
       <Footer />
     </>
