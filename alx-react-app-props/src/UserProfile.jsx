9a89d371339d3ab9566c30bcc0c9b306fb5cd4a8
Jonathan D. Ntim <jonathankweku2@gmail.com>
2025-11-11 14:24:15 +0000

    Rename UserDetails.jsx to UserProfile.jsx for context check compliance and modify UserInfo.jsx

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,16 +1,16 @@
 import { useContext } from 'react';
 import UserContext from './UserContext';
 
-function UserDetails() {
+function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
     <div style={{ border: '1px solid gray', padding: '15px', width: '300px', margin: '20px auto', borderRadius: '8px' }}>
-      <h3>User Details</h3>
+      <h3>User Profile</h3>
       <p><strong>Name:</strong> {userData.name}</p>
       <p><strong>Email:</strong> {userData.email}</p>
     </div>
   );
 }
 
-export default UserDetails;
+export default UserProfile;
