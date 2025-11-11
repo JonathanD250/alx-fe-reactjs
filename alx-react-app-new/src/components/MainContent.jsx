48f9625e823148efc12d73724d95d509610594c3
Jonathan D. Ntim <jonathankweku2@gmail.com>
2025-11-11 11:15:35 +0000

    Add a new directory alx-react-app-new for a new project

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', textAlign: 'center', minHeight: '300px' }}>
+      <h2 style={{ color: '#00796b' }}>Welcome to My City Guide</h2>
+      <p style={{ fontSize: '18px', color: '#004d40' }}>
+        Here you’ll find information about some of my favorite cities around the world.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
