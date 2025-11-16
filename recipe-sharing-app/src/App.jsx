ba31bc9314dc53543a319952c4097e7c824086d5
Jonathan D. Ntim <jonathankweku2@gmail.com>
2025-11-16 16:47:24 +0000

    Modify all file by deleting the old project file and redoing it again

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,28 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeDetails from './components/RecipeDetails'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
+    <>
       <div>
-        <h1>Recipe Sharing App</h1>
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <AddRecipeForm />
-                <RecipeList />
-              </div>
-            }
-          />
-
-          <Route path="/recipes/:id" element={<RecipeDetails />} />
-        </Routes>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </BrowserRouter>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
