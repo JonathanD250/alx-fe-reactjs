dd06253d066eb951a50ae576a1cfe2d2ee8ea75d
Jonathan D. Ntim <jonathankweku2@gmail.com>
2025-12-07 00:11:24 +0000

    Modify recipe-sharing-app/src/App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar'; // Import SearchBar
+import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
     <Router>
       <div>
         <h1>Recipe Sharing Application</h1>
-        {/* Add SearchBar here so it is always visible */}
         <SearchBar />
         <Routes>
           <Route path="/" element={
             <>
               <AddRecipeForm />
+              <FavoritesList />       {/* Added Here */}
+              <RecommendationsList /> {/* Added Here */}
               <RecipeList />
             </>
           } />
